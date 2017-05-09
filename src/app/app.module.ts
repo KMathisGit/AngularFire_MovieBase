343c90a89eefd26fab50012d62afe70566da0a8e
Kevin Mathis <kevinmathis08@gmail.com>
2017-05-09 19:51:42 -0400

    Added Firebase Key and Ng Components
    Added the projects Firebase config information as well as AngularFire's
    Modules that will be needed. Generated email, login, members, and signup
    components for project and tied these to the NgModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyByMPDn94qU_mO9wKbGqV-HOrxpR69lBiA",
+    authDomain: "angularmoviebase.firebaseapp.com",
+    databaseURL: "https://angularmoviebase.firebaseio.com",
+    projectId: "angularmoviebase",
+    storageBucket: "angularmoviebase.appspot.com",
+    messagingSenderId: "359376388067"
+};
+
+// Giving Angular's Module heads up on Components/Modules
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
