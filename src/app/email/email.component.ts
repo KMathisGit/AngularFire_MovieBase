465ae8da5a98130fe19e844797a7a2b3ca795eff
Kevin Mathis <kevinmathis08@gmail.com>
2017-05-10 10:45:08 -0400

    Filled Email Component and Email Form
    Added constructor to Email component and form submission function to
    route client to members page if form submission is successful.
    Validation is included (must be valid email address and at least 6-char
    password).

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-email',
@@ -7,7 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+    state: string = '';
+    error: any;
+
+    constructor(public afAuth: AngularFireAuth,private router: Router) {
+    this.afAuth.authState.subscribe(auth => { 
+      if(auth) {
+        this.router.navigateByUrl('/members');
+      }
+    });
+  }
+
+  // Form Submission: route client to members page if successful
+  onSubmit(formData) {
+    if(formData.valid) {
+      //console.log(formData.value);
+      this.afAuth.auth.signInWithEmailAndPassword(formData.value.email, formData.value.password).
+      then( (success) => { this.router.navigate(['/members']); } ).
+      catch( (err) => { 
+        //console.log(err);
+        this.error = err; 
+      })
+    }
+  }
 
   ngOnInit() {
   }
