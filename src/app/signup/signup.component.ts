87e28a9e6916537bb84dd5b4bd279884eaa0da69
Kevin Mathis <kevinmathis08@gmail.com>
2017-05-10 11:32:46 -0400

    Filled sign-up component and sign-up HTML
    Created sign-up form and implemented sign-up procedure for clients to
    register an email account to the app and store the info in firebase.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  state: string = '';
+  error: any;
+
+  constructor(public afAuth: AngularFireAuth,private router: Router) {
+
+  }
+
+  // Create account submission, if successful route to login page
+  onSubmit(formData) {
+    if(formData.valid) {
+      //console.log(formData.value);
+      this.afAuth.auth.createUserWithEmailAndPassword(formData.value.email, formData.value.password).
+      then( (success) => {
+        //console.log(success);
+        this.router.navigate(['/login'])
+      }).catch(
+        (err) => {
+        //console.log(err);
+        this.error = err;
+      })
+    }
+  }
 
   ngOnInit() {
   }
